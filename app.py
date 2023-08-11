65f4ede11811ba052e8fa3d72ceb58e88084f604
nigrub <ajkybert@gmail.com>
2023-08-11 11:59:17 +0100

    Error handling

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ st.set_page_config(page_title="Your App Title", layout="wide")
 
 PAGES = {
     "Home": "home",  # added this line for the home page
-    "PDF Chat": PDF_Chat,
-    "Test 3": test_3
+    "PDF Co-Pilot": PDF_Chat,
+    "CUPRA Co-Pilot": test_3
 }
 
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 if selection == "Home":
     st.title("Welcome To The App")
-    st.write("Please select a page from the sidebar to navigate.")
+    st.write("Please select a Co-Pilot from the sidebar to navigate.")
 else:
     page = PAGES[selection]
     page.app()
