a8f3bf18fff70c3182627fba750c732159cc4d73
nigrub <ajkybert@gmail.com>
2023-09-25 00:54:09 +0100

    New Files Added For New POC

--- app2.py
+++ app2.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 # Import other pages
-from pages import Powerpoint_Creation_Tool, CSV_Summary, CSV_Analyser
-import Document_Chatter
+from pages import CSV_Summary, CSV_Analyser
 
 st.set_page_config(page_title="Your App Title", layout="wide")
 
@@ -10,7 +9,6 @@ PAGES = {
     "Home": "home",
     "CSV Analyser": CSV_Analyser,
     "CSV Summary": CSV_Summary,
-    "PowerPoint Creation Tool": Powerpoint_Creation_Tool,
 }
 
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
