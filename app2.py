abcf4d592098c50a5e37b1a90e9716ab78d95bbb
nigrub <ajkybert@gmail.com>
2023-09-25 01:09:08 +0100

    New Files Added For New POC

--- app2.py
+++ app2.py
@@ -12,12 +12,3 @@ PAGES = {
     "CSV Summary": CSV_Summary,
 }
 
-# The `if` condition to check the page selection might be missing.
-# For instance:
-selection = st.sidebar.selectbox("Choose a page:", list(PAGES.keys()))
-
-if selection == "Home":
-    st.write("This is the homepage content.")  # Or whatever you'd like the home page to show.
-else:
-    page.app2()
-
