1ed2fbc3093dc8289fdb5ca42e60001d76e1f43a
nigrub <ajkybert@gmail.com>
2023-09-25 01:06:27 +0100

    New Files Added For New POC

--- app2.py
+++ app2.py
@@ -4,18 +4,10 @@ import streamlit as st
 from pages import CSV_Summary, CSV_Analyser
 
 st.set_page_config(page_title="Your App Title", layout="wide")
+st.title("Welcome To The App")
 
 PAGES = {
     "Home": "home",
     "CSV Analyser": CSV_Analyser,
     "CSV Summary": CSV_Summary,
-}
 
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-
-if selection == "Home":
-    st.title("Welcome To The App")
-    st.write("This is the home page of your app. Please select a page from the sidebar to navigate.")
-else:
-    page = PAGES[selection]
-    page.app2()
