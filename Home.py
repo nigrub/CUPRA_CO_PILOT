e5abd3d3ae837504166a453c063c7bd7703782fd
nigrub <ajkybert@gmail.com>
2023-09-25 01:28:22 +0100

    New Files Added For New POC

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Import other pages
-from pages import CSV_Summary, CSV_Analyser
+from pages import CSV_Summary, CSV_Analyser, PDF_Chat, Powerpoint_Creator
 
 st.set_page_config(page_title="Your App Title", layout="wide")
 st.title("Welcome To The App")
@@ -10,5 +10,7 @@ PAGES = {
     "Home": "home",
     "CSV Analyser": CSV_Analyser,
     "CSV Summary": CSV_Summary,
+    "Power Point Creator": Powerpoint_Creator,
+    "PDF Chat": PDF_Chat,
 }
 
